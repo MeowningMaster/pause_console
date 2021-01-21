64c5dd789b291e31980a1939dd148621be4c949a
Andrii Semenystyi <meowningmaster@gmail.com>
2021-01-21 18:24:29 +0200

    refactor

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,8 @@
 use std::io::{stdin, stdout, Read, Write};
 
-pub fn pause_with_message(message: String) {
+pub fn execute() {
     let mut stdout = stdout();
-    stdout.write(b"\n").unwrap();
-    stdout.write(message.as_bytes()).unwrap();
+    stdout.write(b"\nPress Enter to continue...").unwrap();
     stdout.flush().unwrap();
     stdin().read(&mut [0]).unwrap();
-}
-
-pub fn pause() {
-    pause_with_message(String::from("Press Enter to continue..."));
 }
