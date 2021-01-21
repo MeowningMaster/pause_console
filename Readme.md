90f37f5385e55c1aa9dcd395145208545c864ee0
Andrii Semenystyi <meowningmaster@gmail.com>
2021-01-21 18:47:32 +0200

    polished

--- Readme.md
+++ Readme.md
@@ -1,6 +1,19 @@
 # Usage
 ```rust
-pause();
+use pause_console;
+...
+//pause with "Press Enter to continue..." message
+pause_console::execute();
+
+//pause on a new line
+pause_console::executeln();
+
+//pause with custom message
+pause_console::execute_with_message("Custom message");
 //or
-pause_with_message("message");
+let message = String::from("Custom message");
+pause_console::execute_with_message(message.as_str());
+
+//pause with custom message on a new line
+pause_console::executeln_with_message("Custom message");
 ```
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,26 @@
-use std::io::{stdin, stdout, Read, Write};
+use std::io::{stdin, stdout, Write};
+
+const DEFAULT_MESSAGE: &str = "Press Enter to continue...";
 
 pub fn execute() {
+    execute_with_message(DEFAULT_MESSAGE);
+}
+
+pub fn executeln() {
+    executeln_with_message(DEFAULT_MESSAGE);
+}
+
+pub fn execute_with_message(message: &str) {
     let mut stdout = stdout();
-    stdout.write(b"\nPress Enter to continue...").unwrap();
+    stdout.write(message.as_bytes()).unwrap();
     stdout.flush().unwrap();
-    stdin().read(&mut [0]).unwrap();
+    stdin().read_line(&mut String::new()).unwrap();
+}
+
+pub fn executeln_with_message(message: &str) {
+    let mut stdout = stdout();
+    stdout.write(b"\n").unwrap();
+    stdout.write(message.as_bytes()).unwrap();
+    stdout.flush().unwrap();
+    stdin().read_line(&mut String::new()).unwrap();
 }
