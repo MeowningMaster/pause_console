f0629323f73565c14aed4ce68922df2a0fb78079
纳西妲 · Nahida <yanwenz551@gmail.com>
2024-01-29 01:06:08 +0800

    feat!: format output message
    BREAKING CHANGE: Remove `pause_console_execute` function
    BREAKING CHANGE: Remove `PAUSE_CONSOLE_DEFAULT_MESSAGE` static variable

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,13 @@
-use std::io::{stdin, stdout, Write};
-
-pub const PAUSE_CONSOLE_DEFAULT_MESSAGE: &str = "Press Enter to continue...";
-
-pub fn pause_console_execute(message: &str) {
-    let mut stdout = stdout();
-    stdout.write(message.as_bytes()).unwrap();
-    stdout.flush().unwrap();
-    stdin().read_line(&mut String::new()).unwrap();
-}
-
 #[macro_export]
 macro_rules! pause_console {
-    ($message: expr) => {
-        pause_console_execute($message);
-    };
     () => {
-        pause_console_execute(PAUSE_CONSOLE_DEFAULT_MESSAGE);
+        std::print!("Press Enter to continue...");
+        std::io::Write::flush(&mut std::io::stdout()).unwrap();
+        std::io::stdin().read_line(&mut String::new()).unwrap();
+    };
+    ($($arg:tt)*) => {
+        std::print!($($arg)*);
+        std::io::Write::flush(&mut std::io::stdout()).unwrap();
+        std::io::stdin().read_line(&mut String::new()).unwrap();
     };
 }
